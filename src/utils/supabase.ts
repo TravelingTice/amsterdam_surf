1650997673cce290b616ef4d812ffa467cf8f1ef
Matthijs Kralt <matthijs@webinargeek.com>
2025-04-06 13:02:40 +0200

    change: Hardcode the supabase keys for now

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -1,6 +1,7 @@
 import { createClient } from "@supabase/supabase-js"
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+const supabaseUrl = "https://ilubxwjxatjfwtpjteim.supabase.co"
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlsdWJ4d2p4YXRqZnd0cGp0ZWltIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDM0NDE1NDgsImV4cCI6MjA1OTAxNzU0OH0.L02CVOVVIl_K1BhNjK_3sU8mRedStpx64SkUTGF_-F8"
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
